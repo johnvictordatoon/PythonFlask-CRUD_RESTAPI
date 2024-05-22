fbb82170518d508623fb8b39f8b3346a6c65cc27
John Victor Dato-on <143370209+johnvictordatoon@users.noreply.github.com>
2024-05-22 15:38:19 +0800

    add baseline code

--- api.py
+++ api.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# Required
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "admin5678"
+app.config["MYSQL_DB"] = "vehiclereservation_db"
+
+# Extra configs, optional:
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/path/to/ca-file"}}  # https://mysqlclient.readthedocs.io/user_guide.html#functions-and-attributes
+
+mysql = MySQL(app)
+
 @app.route("/")
-def helloworld():
-    return "<p>Hello World</p>"
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute("""SELECT user, host FROM mysql.user""")
+    rv = cur.fetchall()
+    return str(rv)
 
 if __name__ == "__main__":
-    app.run(debug="True")
+    app.run(debug=True)
